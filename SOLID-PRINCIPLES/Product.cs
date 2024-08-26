4b1feb7d15220d364f965f58f446bb0e424a1c5b
Pablo Lopez <72577998+pablo1207@users.noreply.github.com>
2024-08-26 09:54:37 -0600

    Inicio de la practica

---
+++ SOLID-PRINCIPLES/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace SOLID_PRINCIPLES
+{
+    public class Product
+    {
+        public string Code { get; set; }
+
+        public string Classification { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal IVA { get; set; }
+
+        public decimal IEPS { get; set; }
+    }
+}
---
+++ SOLID-PRINCIPLES/Program.cs
@@ -0,0 +1,32 @@
+﻿using SOLID_PRINCIPLES;
+using System.Text;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var productProcessor = new ProductProcessor(Directory.GetCurrentDirectory() + "\\reporte.txt");
+        MemoryStream stream = getMemoryStreamProducts();
+        productProcessor.ProcessProducts(stream);
+
+        Console.WriteLine("El archivo se creo existosamente");
+        Console.ReadLine();
+
+    }
+
+    private static MemoryStream getMemoryStreamProducts()
+    {
+        string productString =
+            "AAA0045|T1 |cacahuates japoneses 150gr |1 |15.00  \n" +
+            "AAA0046|T3 |cacahuates japoneses 150gr |2 |15.00  \n" +
+            "AAA0047|A1 |cacahuates japoneses 150gr |4 |15.00  \n" +
+            "AAA0048|A4 |cacahuates japoneses 150gr |5 |15.00  \n" +
+            "AAA0049|B0 |cacahuates japoneses 150gr |1 |15.00  ";
+
+        // convert string to stream
+        byte[] byteArray = Encoding.ASCII.GetBytes(productString);
+        MemoryStream stream = new MemoryStream(byteArray);
+        return stream;
+    }
+}
